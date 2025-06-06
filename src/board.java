5c0ed4ee796265f69792dc59f251ce47b1167f77
garv130 <13garvsharma@gmail.com>
2025-06-05 20:03:41 -0400

    Update board.java

--- src/board.java
+++ src/board.java
@@ -37,18 +37,29 @@ public class board {
          return false;
         }
 
-        public boolean checkWin(char symbol){
+    public boolean checkWin(char symbol){
             //Check if there is a winner in a row
-           for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++){
             if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol){
                 return true;
             }
            }
-           for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++){
             //Check the columns
             if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol){
                 return true;
             }
            }
+    // Check the diagonal for winners
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol){
+            return true;
+        }
+        
+
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol){
+            return true;
+        }
+
+        return false;
     }   
  }
