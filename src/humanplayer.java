0353249dc6cf3758a629118723bd62b0142c66a3
garv130 <13garvsharma@gmail.com>
2025-06-07 23:27:37 -0400

    Update humanplayer.java

--- src/humanplayer.java
+++ src/humanplayer.java
@@ -3,4 +3,14 @@ import java.util.Scanner;
 public class humanplayer implements player{
     private char symbol;
     private Scanner input;
+
+    public humanplayer(char symbol) {
+        this.symbol = symbol;
+        this.input = new Scanner(System.in);
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
 }
