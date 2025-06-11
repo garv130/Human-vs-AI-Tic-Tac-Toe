7bc229826849d861826d3795efebaf38a7497521
garv130 <13garvsharma@gmail.com>
2025-06-11 15:24:06 -0400

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Board board = new Board();
+        Random rand = new Random();
 
         System.out.print("Do you want to be X or O? (X goes first): ");
         char symbol = scanner.nextLine().trim().toUpperCase().charAt(0);
@@ -19,5 +21,13 @@ public class TicTacToe {
         } else {
             aiSymbol = 'X';
         }
+
+        System.out.println("You are " + symbol + ". AI is " + aiSymbol + ".");
+        
+        //Guessing number system to decide who goes first
+        int correctNumber = rand.nextInt(2) + 1;
+
+        System.out.print("Guess a number (1 or 2) to decide who goes first: ");
+        int guess = scanner.nextInt();
     }
 }
