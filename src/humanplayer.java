ee921470e2903e35ed6c6e12dda218e882c0de2b
garv130 <13garvsharma@gmail.com>
2025-06-10 21:58:29 -0400

    Update humanplayer.java

--- src/humanplayer.java
+++ src/humanplayer.java
@@ -17,6 +17,11 @@ public class humanplayer implements player{
         boolean success = false;
         while (!success){
             System.out.println("Player " + symbol + ", enter your move (row and column 0–2):");
+            success = board.spotMarker(row, col, symbol);
+            if (!success){
+                System.out.println("Invalid move. Please try again.");
+            }
+        
         }
 
     }
